a340118a59701baffd495845e1fb17061414cc55
C6H5FJokey <60382052+C6H5FJokey@users.noreply.github.com>
2022-06-25 18:39:54 +0800

    +添加顺序表的储存结构

---
+++ SqList.cpp
@@ -0,0 +1,141 @@
+#include "SqList.h"
+#include <stdlib.h>
+#include <string.h>
+
+Status InitList_Sq(SqList& L)
+{
+    L.elem = (SqNode*)malloc(LIST_INIT_SIZE * sizeof(void*));
+    if (!L.elem)exit(OVERFLOW);
+    L.length = 0;
+    L.listsize = LIST_INIT_SIZE;
+    return OK;
+}
+
+Status ClearList_Sq(SqList& L)
+{
+    int i;
+    for (i = 0; i < L.length; i++)
+    {
+        free(L.elem[i].data);
+        L.elem[i].data = NULL;
+        L.elem[i].n = 0;
+    }
+    L.length = 0;
+    return OK;
+}
+
+Status DestroyList_Sq(SqList& L)
+{
+    ClearList_Sq(L);
+    free(L.elem);
+    L.elem = NULL;
+    L.length = 0;
+    L.listsize = 0;
+    return OK;
+}
+
+Status Append_Sq(SqList& L, const void* e, size_t n)
+{
+    SqNode* newbase;
+    char* buffer;
+    if (L.length >= L.listsize) {
+        newbase = (SqNode*)realloc(L.elem, sizeof(void*) * L.listsize * 2);
+        if (!newbase) exit(OVERFLOW);
+        L.elem = newbase;
+        L.listsize *= 2;
+    }
+    buffer = (char*)malloc(n);
+    if (!buffer)exit(OVERFLOW);
+    memcpy(buffer, e, n);
+    L.elem[L.length].data = buffer;
+    L.elem[L.length].n = n;
+    L.length++;
+    return OK;
+}
+
+Status ListInsert_Sq(SqList& L, int i, const void* e, size_t n)
+{
+    SqNode* newbase;
+    SqNode* p;
+    SqNode* q;
+    char* buffer;
+    if (i < 1 || i > L.length + 1)return ERROR;
+    if (L.length >= L.listsize) {
+        newbase = (SqNode*)realloc(L.elem, sizeof(void*) * L.listsize * 2);
+        if (!newbase) exit(OVERFLOW);
+        L.elem = newbase;
+        L.listsize *= 2;
+    }
+    buffer = (char*)malloc(n);
+    if (!buffer)exit(OVERFLOW);
+    memcpy(buffer, e, n);
+    q = &(L.elem[i - 1]);
+    for (p = &(L.elem[L.length - 1]); p >= q; --p)*(p + 1) = *p;
+    q->data = buffer;
+    q->n = n;
+    L.length++;
+    return OK;
+}
+
+Status Remove_Sq(SqList& L, void* e)
+{
+    memcpy(e, L.elem[L.length].data, L.elem[L.length].n);
+    free(L.elem[L.length].data);
+    L.elem[L.length].data = NULL;
+    L.elem[L.length].n = 0;
+    L.length--;
+    return OK;
+}
+
+Status ListDelete_Sq(SqList& L, int i, void* e)
+{
+    SqNode* p;
+    SqNode* q;
+    if (i<1 || i > L.length) return ERROR;
+    p = &(L.elem[i - 1]);
+    memcpy(e, p->data, p->n);
+    free(p->data);
+    q = L.elem + L.length - 1;
+    for (++p; p <= q; ++p) *(p - 1) = *p;
+    q->data = NULL;
+    q->n = 0;
+    L.length--;
+    return OK;
+}
+
+Status GetElem_Sq(SqList L, int i, void* e)
+{
+    if (i<1 || i > L.length) return ERROR;
+    memcpy(e, L.elem[i].data, L.elem[i].n);
+    return OK;
+}
+
+Status SetElem_Sq(SqList L, int i, const void* e, size_t n)
+{
+    char* buffer;
+    if (i<1 || i > L.length) return ERROR;
+    if (L.elem[i].n == n) 
+    {
+        buffer = (char*)L.elem[i].data;
+    }
+    else
+    {
+        buffer = (char*)malloc(n);
+        if (!buffer)exit(OVERFLOW);
+        free(L.elem[i].data);
+        L.elem[i].n = n;
+    }
+    memcpy(buffer, e, n);
+    L.elem[i].data = buffer;
+    return OK;
+}
+
+Status ListTraverse_Sq(SqList L, Status(*visit)(const void*, void*), void* addition)
+{
+    int i;
+    for (i = 0; i < L.length; i++)
+    {
+        if (!visit(L.elem[i].data, addition)) return ERROR;
+    }
+    return OK;
+}
---
+++ SqList.h
@@ -0,0 +1,57 @@
+#ifndef __SQ_LIST_H__
+#define __SQ_LIST_H__
+
+#define TRUE 1
+#define FALSE 0
+#define OVERFLOW -2
+#define OK 1
+#define ERROR 0
+
+#define LIST_INIT_SIZE 14
+
+typedef int Status;
+
+typedef struct
+{
+	void* data;
+	size_t n;
+}SqNode;
+
+typedef struct
+{
+	SqNode* elem;
+	int length;
+	int listsize;
+}SqList;
+
+
+//函数：初始化一个链表，实际上就是创建一个头结点
+Status InitList_Sq(SqList& L);
+
+//函数：清空一个链表
+Status ClearList_Sq(SqList& L);
+
+//函数：销毁一个链表
+Status DestroyList_Sq(SqList& L);
+
+//函数：将元素e插在列表最后面
+Status Append_Sq(SqList& L, const void* e, size_t n);
+
+//函数：在第i位前插入一个元素e，n为元素e的空间大小
+Status ListInsert_Sq(SqList& L, int i, const void* e, size_t n);
+
+//函数：将列表最后面的元素删除并返回元素e
+Status Remove_Sq(SqList& L, void* e);
+
+//函数：删除表中第i个元素
+Status ListDelete_Sq(SqList& L, int i, void* e);
+
+//函数：获得表中第i个元素
+Status GetElem_Sq(SqList L, int i, void* e);
+
+//函数：修改表中第i个元素为e，n为元素e的空间大小
+Status SetElem_Sq(SqList L, int i, void* e, size_t n);
+
+//遍历整个表 可以附加额外数据
+Status ListTraverse_Sq(SqList L, Status(*visit)(const void*, void*), void* addition);
+#endif // !__SQ_LIST_H__
